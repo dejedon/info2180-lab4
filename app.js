8db2947d98383b8cb5bbfb144d9694685b38ea77
Dejean Whyte <dejeanwhyte@gmail.com>
2023-11-10 16:31:33 -0500

    Completed Exercise 3

--- app.js
+++ app.js
@@ -9,15 +9,14 @@ window.onload = function(){
         console.log("click");
 
         var httpRequest = new XMLHttpRequest();
-        httpRequest.open('GET','http://localhost:8888/info2180-lab4/superheroes.php' ); //?query='+inputfield.value, true);
+        httpRequest.open('GET','http://localhost:8888/info2180-lab4/superheroes.php?query='+inputfield.value, true);
         httpRequest.send();
    
         httpRequest.onreadystatechange = function(){
-            if(httpRequest.readyState === XMLHttpRequest.DONE){
+            if(httpRequest.readyState == XMLHttpRequest.DONE){
                 if(httpRequest.status == 200) {
                     var results = httpRequest.responseText;
-                   //result.innerHTML=results
-                   alert(results);
+                    result.innerHTML=results
                 } 
                 else{
                 alert("Error");
