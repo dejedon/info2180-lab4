c9e92852f5e7c800a9dab645406008a3b2363e11
Dejean Whyte <dejeanwhyte@gmail.com>
2023-11-10 11:56:49 -0500

    Completed Exercise 2

---
+++ app.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+    
+    var search = document.getElementById("btn-search");
+    var inputfield = document.getElementById("search");
+    var result = document.getElementById("result");
+    
+    search.addEventListener("click", (e)=>{
+        e.preventDefault();
+        console.log("click");
+
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.open('GET','http://localhost:8888/info2180-lab4/superheroes.php' ); //?query='+inputfield.value, true);
+        httpRequest.send();
+   
+        httpRequest.onreadystatechange = function(){
+            if(httpRequest.readyState === XMLHttpRequest.DONE){
+                if(httpRequest.status == 200) {
+                    var results = httpRequest.responseText;
+                   //result.innerHTML=results
+                   alert(results);
+                } 
+                else{
+                alert("Error");
+                }
+            }
+        }
+    })
+}
